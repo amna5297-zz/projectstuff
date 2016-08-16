4a1dfd5d65b3e16c6d422dc8eaab4c97854771d1
alifarooq96 <alifarooq96@gmail.com>
2016-08-16 22:26:17 +0500

    email changes complete
    no description

--- myassets/js/login/switch.js
+++ myassets/js/login/switch.js
@@ -2,11 +2,17 @@ $(document).ready(function(){
     $("#createaccountbtn").click(function(){
         displayRegistrationForm();
     });
-    
+
     $("#loginbtn").click(function(){
         displayLoginForm();
-    })
-   
+    });
+
+    $("#backtologin").click(function(){
+      $("#registersuccess").hide("600", function(){
+          $("#mainlogin").show("600");
+      });
+    });
+
 });
 
 function displayRegistrationForm(){
